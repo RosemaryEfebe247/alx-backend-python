5bab924411f73fc7f230b3a156c079ac74a0c977
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-08 18:36:03 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type annotated function which takes
+floating number  n  and returns the floor(int) of n
+"""
+
+
+def floor(n: float) -> int:
+    """Function that returns the int of a float number
+    """
+    return int(n)
