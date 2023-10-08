364bc8d95516c47cf5b2896b86685bbd1e6304d6
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-08 19:43:30 +0100

    sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type annotated function
+Return the sum of the array in float
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """Function that returns the sum of a list
+    """
+    return sum(input_list)
