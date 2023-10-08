5611d382676d08f43329d068d253ec62c348ebc3
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-08 18:18:37 +0100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This function writes a type annotated function
+to concat two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenate str1 and str2
+    returns the concatenated string
+    """
+    return str1 + str2
