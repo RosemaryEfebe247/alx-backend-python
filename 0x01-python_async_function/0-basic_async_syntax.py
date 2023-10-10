42e85132f8a3065b0af1ba0c770a03a022441045
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-10 02:59:35 +0100

    basic_async_syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay=10):
     """
     Args:
         max_delay: integer value of input set to default 10
