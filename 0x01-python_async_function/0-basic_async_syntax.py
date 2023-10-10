c709e570efc0b9ca76d67abb5cf678b323a909e4
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-10 02:58:11 +0100

    basic_async_syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This function is an asynchronous function
+Its takes in an int or a float and delays for a period from 0-int
+returns the int
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """
+    Args:
+        max_delay: integer value of input set to default 10
+    delay:
+        Generates random number of int or float
+        delay for a duration between 0 and max_delay
+    return:
+        return the random number
+    """
+    rand_num = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_num)
+    return rand_num
